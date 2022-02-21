1f92666862fdcbc457703fd22b3b28d20c77fb80
karanja2 <81411892+karanja2@users.noreply.github.com>
2022-02-21 09:22:12 +0300

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,22 @@
+String1 = "{0:b}".format(16)
+print("\nBinary representation of 16 is ")
+print(String1)
+ 
+ # Formatting of Floats
+String1 = "{0:e}".format(165.6458)
+print("\nExponent representation of 165.6458 is ")
+print(String1)
+ 
+# Rounding off Integers
+String1 = "{0:.2f}".format(1/6)
+print("\none-sixth is : ")
+print(String1)
+
+# String alignment
+String1 = "|{:<10}|{:^10}|{:>10}|".format('Geeks', 'for', 'Geeks')
+print("\nLeft, center and right alignment with Formatting: ")
+print(String1)
+
+# To demonstrate aligning of spaces
+String1 = "\n{0:^16} was founded in {1:<4}!".format("GeeksforGeeks", 2009)
+print(String1)
